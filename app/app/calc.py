3d08358291586d90c00f88db0937607acf3ce25d
egyping <fahdkuwait@gmail.com>
2020-05-15 17:49:42 +0300

    flake8 works perfect with tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,5 +1,10 @@
 
 
 def add(x, y):
-    
+    """ test case 1 """
     return x + y
+
+
+def subtract(x, y):
+    """ test case 2 """
+    return (x - y)
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, subtract
 
 
-class CalcCase(TestCase):
-    
-    def test_add_number(self):
-        
-        self.assertEqual(add(5 , 15), 20)
+class CalcTests(TestCase):
+    def test_add_numbers(self):
+        """ test case 1 """
+        self.assertEqual(add(5, 15), 20)
+
+    def test_subtract_number(self):
+        """ test case 2 """
+        self.assertEqual(subtract(8, 2), 6)
