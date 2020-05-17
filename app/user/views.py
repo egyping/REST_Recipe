ee0db7ebe982cfc397416c688a6bdd209dbbf253
egyping <fahdkuwait@gmail.com>
2020-05-18 00:11:55 +0300

    create and modify user with token completed

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 
 from user.serializers import UserSerializer, AuthTokenSerializer
 
@@ -16,3 +16,13 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for the user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
